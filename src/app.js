e905765a870373e3dc5c062259f6275276563a7f
andrewflower-rs <arnaldomahone@hotmail.com>
2024-03-20 23:15:59 -0300

    agrego los modulos necesesarios

--- src/app.js
+++ src/app.js
@@ -1,28 +1,33 @@
 const express = require('express');
-const app = express()       
- const path = require('path')
- const port = 3030
+const app = express()
+const path = require('path')
+var cookieParser = require('cookie-parser');
+const session = require("express-session")
 
- //Configs
- app.set("view engine" , "ejs")
- app.set("views" .path.join(__dirname),"./views")
+const port = 3030
 
-          //Middlewares
-        
+//Configs
+app.set("view engine", "ejs")
+app.set("views".path.join(__dirname), "./views")
+
+//Middlewares
+
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser());
+app.use(session({ secret: "palabra secreta" }))
 
-           //Rutas 
-    const otherRutes = require('./controllers/other')
+//Rutas 
+const otherRutes = require('./controllers/other')
 const authRutes = require('./controllers/authentication')
 
-        //Enrutadore
+//Enrutadores
 
-    app.use("/", otherRutes)
-    app.use("/ath",authRutes)
-
-
+app.use("/", otherRutes)
+app.use("/ath", authRutes)
 
 
- //Levantamos ewl servidor
 
- app.listen(port , () => console.log(`http://localhost:${port}`))
+
+//Levantamos ewl servidor
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
